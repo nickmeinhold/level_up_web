1f94ee066c571a6c8bf8dbcfc2aa471ccb3e202c
Nick Meinhold <nick@enspyr.co>
2025-05-15 15:18:59 +1000

    Add firebase functions

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,36 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:level_up_shared/level_up_shared.dart';
 import 'package:level_up_web/screens/home_screen.dart';
 import 'package:level_up_web/screens/program_details_screen.dart';
 
+final _router = GoRouter(
+  routes: [
+    GoRoute(
+      path: '/',
+      builder: (context, state) => const HomeScreen(),
+      routes: [
+        GoRoute(
+          path: 'program/:id',
+          builder:
+              (context, state) =>
+                  ProgramDetailsScreen(programId: state.pathParameters['id']!),
+        ),
+      ],
+    ),
+  ],
+);
+
 void main() {
+  // Setup the data layer of the "data layer architecture"
+  final firestore = FirebaseFirestore.instance;
+  final auth = FirebaseAuth.instance;
+
+  // Add services to the Locator
+  Locator.add<AuthService>(AuthService(auth: auth, firestore: firestore));
+
   runApp(const LevelUpApp());
 }
 
@@ -23,20 +50,3 @@ class LevelUpApp extends StatelessWidget {
     );
   }
 }
-
-final _router = GoRouter(
-  routes: [
-    GoRoute(
-      path: '/',
-      builder: (context, state) => const HomeScreen(),
-      routes: [
-        GoRoute(
-          path: 'program/:id',
-          builder:
-              (context, state) =>
-                  ProgramDetailsScreen(programId: state.pathParameters['id']!),
-        ),
-      ],
-    ),
-  ],
-);
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:chewie/chewie.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:level_up_shared/level_up_shared.dart';
 import 'package:level_up_web/widgets/faq_section.dart';
 import 'package:level_up_web/widgets/lmowledge_sign_up_section.dart';
 import 'package:level_up_web/widgets/pricing_section.dart';
@@ -74,15 +75,25 @@ class _HomeScreenState extends State<HomeScreen> {
       appBar: AppBar(
         title: const Text('LevelUp Coaching'),
         actions: [
-          TextButton(
-            onPressed: () => context.push('/signin'),
-            child: const Text('Sign In', style: TextStyle(color: Colors.white)),
-          ),
-          const SizedBox(width: 8),
-          ElevatedButton(
-            onPressed: () => context.push('/signup'),
-            child: const Text('Sign Up'),
-          ),
+          if (locate<AuthService>().currentUserId == null) ...[
+            TextButton(
+              onPressed: () => context.push('/signin'),
+              child: const Text(
+                'Sign In',
+                style: TextStyle(color: Colors.black),
+              ),
+            ),
+            const SizedBox(width: 8),
+            ElevatedButton(
+              onPressed: () => context.push('/signup'),
+              child: const Text('Sign Up'),
+            ),
+          ],
+          if (locate<AuthService>().currentUserId == null)
+            ElevatedButton(
+              onPressed: () => context.push('/account'),
+              child: const Text('Account'),
+            ),
           PopupMenuButton<String>(
             itemBuilder:
                 (context) => [
@@ -204,120 +215,6 @@ class _HomeScreenState extends State<HomeScreen> {
           ],
         ),
       ),
-    );
-  }
-}
-
-class _ControlsOverlay extends StatelessWidget {
-  const _ControlsOverlay({required this.controller});
-
-  static const List<Duration> _exampleCaptionOffsets = <Duration>[
-    Duration(seconds: -10),
-    Duration(seconds: -3),
-    Duration(seconds: -1, milliseconds: -500),
-    Duration(milliseconds: -250),
-    Duration.zero,
-    Duration(milliseconds: 250),
-    Duration(seconds: 1, milliseconds: 500),
-    Duration(seconds: 3),
-    Duration(seconds: 10),
-  ];
-  static const List<double> _examplePlaybackRates = <double>[
-    0.25,
-    0.5,
-    1.0,
-    1.5,
-    2.0,
-    3.0,
-    5.0,
-    10.0,
-  ];
-
-  final VideoPlayerController controller;
-
-  @override
-  Widget build(BuildContext context) {
-    return Stack(
-      children: <Widget>[
-        AnimatedSwitcher(
-          duration: const Duration(milliseconds: 50),
-          reverseDuration: const Duration(milliseconds: 200),
-          child:
-              controller.value.isPlaying
-                  ? const SizedBox.shrink()
-                  : const ColoredBox(
-                    color: Colors.black26,
-                    child: Center(
-                      child: Icon(
-                        Icons.play_arrow,
-                        color: Colors.white,
-                        size: 100.0,
-                        semanticLabel: 'Play',
-                      ),
-                    ),
-                  ),
-        ),
-        GestureDetector(
-          onTap: () {
-            controller.value.isPlaying ? controller.pause() : controller.play();
-          },
-        ),
-        Align(
-          alignment: Alignment.topLeft,
-          child: PopupMenuButton<Duration>(
-            initialValue: controller.value.captionOffset,
-            tooltip: 'Caption Offset',
-            onSelected: (Duration delay) {
-              controller.setCaptionOffset(delay);
-            },
-            itemBuilder: (BuildContext context) {
-              return <PopupMenuItem<Duration>>[
-                for (final Duration offsetDuration in _exampleCaptionOffsets)
-                  PopupMenuItem<Duration>(
-                    value: offsetDuration,
-                    child: Text('${offsetDuration.inMilliseconds}ms'),
-                  ),
-              ];
-            },
-            child: Padding(
-              padding: const EdgeInsets.symmetric(
-                // Using less vertical padding as the text is also longer
-                // horizontally, so it feels like it would need more spacing
-                // horizontally (matching the aspect ratio of the video).
-                vertical: 12,
-                horizontal: 16,
-              ),
-              child: Text('${controller.value.captionOffset.inMilliseconds}ms'),
-            ),
-          ),
-        ),
-        Align(
-          alignment: Alignment.topRight,
-          child: PopupMenuButton<double>(
-            initialValue: controller.value.playbackSpeed,
-            tooltip: 'Playback speed',
-            onSelected: (double speed) {
-              controller.setPlaybackSpeed(speed);
-            },
-            itemBuilder: (BuildContext context) {
-              return <PopupMenuItem<double>>[
-                for (final double speed in _examplePlaybackRates)
-                  PopupMenuItem<double>(value: speed, child: Text('${speed}x')),
-              ];
-            },
-            child: Padding(
-              padding: const EdgeInsets.symmetric(
-                // Using less vertical padding as the text is also longer
-                // horizontally, so it feels like it would need more spacing
-                // horizontally (matching the aspect ratio of the video).
-                vertical: 12,
-                horizontal: 16,
-              ),
-              child: Text('${controller.value.playbackSpeed}x'),
-            ),
-          ),
-        ),
-      ],
     );
   }
 }
