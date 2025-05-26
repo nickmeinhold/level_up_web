bdabd96126940798df11224afc85f74748ffb42d
Nick Meinhold <nick@enspyr.co>
2025-05-26 16:16:48 +1000

    Update dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:level_up_shared/level_up_shared.dart';
+import 'package:level_up_web/firebase_options.dart';
 import 'package:level_up_web/screens/home_screen.dart';
 import 'package:level_up_web/screens/program_details_screen.dart';
 
@@ -23,7 +25,10 @@ final _router = GoRouter(
   ],
 );
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
   // Setup the data layer of the "data layer architecture"
   final firestore = FirebaseFirestore.instance;
   final auth = FirebaseAuth.instance;
